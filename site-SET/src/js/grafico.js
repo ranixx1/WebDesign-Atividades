f43b73b5e5020994c0b27807db0380999b42a9f0
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-11 09:42:51 -0300

    feat: adiciona script do logout

--- site-SET/src/js/grafico.js
+++ site-SET/src/js/grafico.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-function redirecionar(){
-  window.location("http://127.0.0.1:5500/site-SET/index.html")
+function redirecionar() {
+    window.location.href = 'http://127.0.0.1:5500/site-SET/login.html';
 }
 
